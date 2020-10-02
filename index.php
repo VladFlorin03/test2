b08f4d785758aa82f2fb89fea850ca85d832bde1
VladFlorin03 <vladf03ac41@gmail.com>
2020-10-02 11:56:13 +0200

    test incarcare

---
+++ index.php
@@ -0,0 +1,15 @@
+   
+   <?php 
+        include 'includes/header.php' 
+    ?>
+    <!-- Basic HTML -->
+    <h1>Hey PHP Prim</h1>
+    <br/>
+    
+    <button type="button" class="btn btn-dark">CLICK ME!</button>
+    <button type="button" class="btn btn-primary">CLICK ME!</button>
+    <button type="button" class="btn btn-success">CLICK ME!</button>
+   <!-- <a href="https://www.heroku.com" target="_blank" class="btn btn-danger">Heroku.com</a>-->
+    <?php
+        require 'includes/footer.php' 
+    ?>
